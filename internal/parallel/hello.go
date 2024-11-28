7737bd06301a669d46538847f37c5a53e3a812a3
AleksandraShmakova <113943664+AleksandraShmakova@users.noreply.github.com>
2024-11-28 14:21:03 +0300

    Update hello.go

--- internal/parallel/hello.go
+++ internal/parallel/hello.go
@@ -2,17 +2,23 @@ package parallel
 
 import (
 	"fmt"
-	"github.com/AleksandraShmakova/backend/pkg/mutex"
+	"github.com/username/mymutexproject/pkg/mutex"
+	"strings"
 )
 
-func Say() {
+// Say выполняет горутины и возвращает результаты их работы как строку.
+func Say() string {
+	var result strings.Builder
 	m := mutex.NewMutex(3)
 
+	// Запуск горутин
 	for i := 0; i < 3; i++ {
 		go func(i int) {
 			defer m.Unlock()
-			fmt.Printf("Горутина %d: Привет, мир!\n", i)
+			result.WriteString(fmt.Sprintf("Горутина %d: Привет, мир!\n", i))
 		}(i)
 	}
 	m.Wait()
+
+	return result.String()
 }
