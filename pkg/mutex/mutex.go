d529b8661f7d990fec91e072b7e99c39523820c2
AleksandraShmakova <113943664+AleksandraShmakova@users.noreply.github.com>
2024-11-28 14:09:15 +0300

    Update mutex.go

--- pkg/mutex/mutex.go
+++ pkg/mutex/mutex.go
@@ -1,21 +1,23 @@
 package mutex
 
 type Mutex struct {
-	ch chan struct{}
+	Count  int
+	signal chan struct{}
 }
 
-func NewMutex() *Mutex {
-	return &Mutex{ch: make(chan struct{}, 1)}
-}
-
-func (m *Mutex) Lock() {
-	m.ch <- struct{}{}
+func NewMutex(count int) *Mutex {
+	return &Mutex{
+		Count:  count,
+		signal: make(chan struct{}, count),
+	}
 }
 
 func (m *Mutex) Unlock() {
-	select {
-	case <-m.ch:
-	default:
-		panic("unlock of unlocked mutex")
+	m.signal <- struct{}{}
+}
+
+func (m *Mutex) Wait() {
+	for i := 0; i < m.Count; i++ {
+		<-m.signal
 	}
 }
