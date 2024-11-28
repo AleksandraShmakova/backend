9511391e160c4221bdd2113d3fb67c72cfda559f
AleksandraShmakova <113943664+AleksandraShmakova@users.noreply.github.com>
2024-11-28 12:12:54 +0300

    Create hello.go

---
+++ internal/parallel/hello.go
@@ -0,0 +1,22 @@
+package parallel
+
+import (
+	"fmt"
+	"github.com/username/mutex-demo/pkg/mutex"
+)
+
+func Say() {
+	m := mutex.NewMutex()
+
+	m.Lock()
+	fmt.Println("Locked the mutex")
+	go func() {
+		defer m.Unlock()
+		fmt.Println("Unlocked from goroutine")
+	}()
+
+	m.Lock() // Ждем, пока горутина освободит мьютекс
+	fmt.Println("Locked again")
+	m.Unlock()
+	fmt.Println("Unlocked the mutex")
+}
